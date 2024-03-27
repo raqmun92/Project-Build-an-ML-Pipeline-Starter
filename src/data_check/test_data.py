f15d5b81c2ce59dbd695df7425700563097c1e0c
raqmun92 <rmuno@LAPTOP-EJJV42F1>
2024-03-27 12:46:01 -0700

    Added tests to test_data file

--- src/data_check/test_data.py
+++ src/data_check/test_data.py
@@ -52,7 +52,7 @@ def test_proper_boundaries(data: pd.DataFrame):
 def test_similar_neigh_distrib(data: pd.DataFrame, ref_data: pd.DataFrame, kl_threshold: float):
     """
     Apply a threshold on the KL divergence to detect if the distribution of the new data is
-    significantly different than that of the reference dataset
+    significantly different from that of the reference dataset
     """
     dist1 = data['neighbourhood_group'].value_counts().sort_index()
     dist2 = ref_data['neighbourhood_group'].value_counts().sort_index()
@@ -60,6 +60,15 @@ def test_similar_neigh_distrib(data: pd.DataFrame, ref_data: pd.DataFrame, kl_th
     assert scipy.stats.entropy(dist1, dist2, base=2) < kl_threshold
 
 
-########################################################
-# Implement here test_row_count and test_price_range   #
-########################################################
+def test_row_count(data):
+    """
+    Make sure the dataset is a reasonable size
+    """
+    assert 15000 < data.shape[0] < 1000000
+
+
+def test_price_range(data, min_price, max_price):
+    """
+    Make sure the prices are within a certain range
+    """
+    assert data['price'].between(min_price, max_price).all()
